1ab52bf5eadba58568e81d8666ed1a70ced80720
gaoyang.wu <gaoyang.wu@qq.com>
2024-10-05 00:07:19 +0800

    update

---
+++ _worker.js
@@ -0,0 +1,117 @@
+/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run "npm run dev" in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run "npm run deploy" to publish your worker
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
+
+
+export default {
+    async fetch(request, env, ctx) {
+        return new Response('cf.090227.xyz#优选官方线路\n' +
+            'time.cloudflare.com\n' +
+            'shopify.com\n' +
+            'time.is\n' +
+            'icook.hk\n' +
+            'icook.tw\n' +
+            'ip.sb\n' +
+            'japan.com\n' +
+            'malaysia.com\n' +
+            'russia.com\n' +
+            'singapore.com\n' +
+            'skk.moe\n' +
+            'www.visa.com\n' +
+            'www.visa.com.sg\n' +
+            'www.visa.com.hk\n' +
+            'www.visa.com.tw\n' +
+            'www.visa.co.jp\n' +
+            'www.visakorea.com\n' +
+            'www.gco.gov.qa\n' +
+            'www.gov.se\n' +
+            'www.gov.ua\n' +
+            'www.digitalocean.com\n' +
+            'www.csgo.com\n' +
+            'www.shopify.com\n' +
+            'www.whoer.net\n' +
+            'www.whatismyip.com\n' +
+            'www.ipget.net\n' +
+            'www.hugedomains.com\n' +
+            'www.udacity.com\n' +
+            'www.4chan.org\n' +
+            'www.okcupid.com\n' +
+            'www.glassdoor.com\n' +
+            'www.udemy.com\n' +
+            'www.baipiao.eu.org\n' +
+            'cdn.anycast.eu.org\n' +
+            'cdn-all.xn--b6gac.eu.org\n' +
+            'cdn-b100.xn--b6gac.eu.org\n' +
+            'xn--b6gac.eu.org\n' +
+            'edgetunnel.anycast.eu.org\n' +
+            'alejandracaiccedo.com\n' +
+            'nc.gocada.co\n' +
+            'log.bpminecraft.com\n' +
+            'www.boba88slot.com\n' +
+            'gur.gov.ua\n' +
+            'www.zsu.gov.ua\n' +
+            'www.iakeys.com\n' +
+            'edtunnel-dgp.pages.dev\n' +
+            'www.d-555.com\n' +
+            'fbi.gov\n' +
+            '69.84.182.125:443#US\n' +
+            '198.62.62.39:443#US\n' +
+            '170.39.66.154:443#US\n' +
+            '198.62.62.198:443#US\n' +
+            '198.62.62.137:443#US\n' +
+            '69.84.182.171:443#US\n' +
+            '43.153.103.203:443#US\n' +
+            '160.79.104.66:443#US\n' +
+            '198.62.62.37:443#US\n' +
+            '160.123.255.10:443#ZA\n' +
+            '130.250.137.114:443#US\n' +
+            '69.84.182.30:443#US\n' +
+            '192.0.54.104:443#US\n' +
+            '69.84.182.145:443#US\n' +
+            '45.67.215.11:443#RU\n' +
+            '69.84.182.38:443#US\n' +
+            '219.76.13.164:443#HK\n' +
+            '8.210.138.40:443#HK\n' +
+            '219.76.13.169:443#HK\n' +
+            '219.76.13.183:443#HK\n' +
+            '61.239.234.5:35500#HK\n' +
+            '223.18.129.85:11524#HK\n' +
+            '131.186.22.39:34237#KR\n' +
+            '222.120.203.187:34337#KR\n' +
+            '211.228.234.170:12134#KR\n' +
+            '1.247.157.224:30012#KR\n' +
+            '185.238.249.98:3306#US\n' +
+            '45.32.81.251:8444#US\n' +
+            '38.76.148.89:55555#US\n' +
+            '38.58.63.68:54321#US\n' +
+            '118.163.93.53:81#TW\n' +
+            '129.146.111.62:443#US\n' +
+            '152.67.212.141:443#KR\n' +
+            '154.19.186.124:16873#JP\n' +
+            '210.61.97.241:81#TW\n' +
+            '212.50.249.121:443#JP\n' +
+            '34.97.177.229:38906#JP\n' +
+            '40.81.29.103:8089#HK\n' +
+            '43.155.75.187:24657#HK\n' +
+            '64.64.228.98:2053#US\n' +
+            '8.218.197.68:13828#HK\n' +
+            '162.159.153.4:443#CT\n' +
+            '162.159.152.253:443#CT\n' +
+            '162.159.153.66:443#CT\n' +
+            '162.159.153.2:443#CT\n' +
+            '162.159.152.254:443#CT\n' +
+            '162.159.152.226:443#CT\n' +
+            '104.19.47.22:443#CMCC\n' +
+            '104.19.46.47:443#CMCC\n' +
+            '104.19.62.169:443#CMCC\n' +
+            '104.19.43.40:443#CMCC\n' +
+            '104.19.40.46:443#CMCC\n' +
+            '104.19.52.19:443#CMCC');
+    },
+};
