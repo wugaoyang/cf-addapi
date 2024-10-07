70827a560bd87e25021e1a342c1e2761fa6ab35b
gaoyang.wu <gaoyang.wu@qq.com>
2024-10-07 21:24:06 +0800

    update

--- _worker.js
+++ _worker.js
@@ -8,13 +8,13 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
-let URLS='www.ipget.net';
+let URLS = 'www.ipget.net\n';
 
 let US_IPS = '104.17.74.165#自选官方优选US 23.16MB/s\n' +
     '141.101.120.87#自选官方优选US 22.99MB/s\n' +
     '104.16.223.60#自选官方优选US 11.67MB/s\n' +
     '104.16.217.57#自选官方优选US 10.89MB/s\n' +
-    '141.101.122.215#自选官方优选US 9.97MB/s\n';
+    '141.101.122.215#自选官方优选US 9.97MB/s';
 
 let HK_IPS = '156.59.152.144#自选反代优选HK\n' +
     '27.50.49.169#自选反代优选HK\n' +
@@ -23,6 +23,11 @@ let HK_IPS = '156.59.152.144#自选反代优选HK\n' +
     '154.218.15.140#自选反代优选HK';
 export default {
     async fetch(request, env, ctx) {
-        return new Response(URLS + US_IPS + HK_IPS);
+        return new Response(
+            URLS + "\n" +
+            US_IPS + '\n'
+            HK_IPS
+    )
+        ;
     },
 };
