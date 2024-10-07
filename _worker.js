61f0b6460db8024833d46c79c4e4891ed4dbc7db
gaoyang.wu <gaoyang.wu@qq.com>
2024-10-07 20:58:00 +0800

    update

--- _worker.js
+++ _worker.js
@@ -8,13 +8,19 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+let US_IPS = '104.17.74.165#自选官方优选US 23.16MB/s\n' +
+    '141.101.120.87#自选官方优选US 22.99MB/s\n' +
+    '104.16.223.60#自选官方优选US 11.67MB/s\n' +
+    '104.16.217.57#自选官方优选US 10.89MB/s\n' +
+    '141.101.122.215#自选官方优选US 9.97MB/s\n';
+
+let HK_IPS = '156.59.152.144#自选反代优选HK\n' +
+    '27.50.49.169#自选反代优选HK\n' +
+    '27.50.48.112#自选反代优选HK\n' +
+    '27.50.48.60#自选反代优选HK\n' +
+    '154.218.15.140#自选反代优选HK';
 export default {
     async fetch(request, env, ctx) {
-        return new Response(
-            '104.17.74.165#自选官方优选US 23.16MB/s\n' +
-            '141.101.120.87#自选官方优选US 22.99MB/s\n' +
-            '104.16.223.60#自选官方优选US 11.67MB/s\n' +
-            '104.16.217.57#自选官方优选US 10.89MB/s\n' +
-            '141.101.122.215#自选官方优选US 9.97MB/s\n');
+        return new Response(US_IPS + HK_IPS);
     },
 };
